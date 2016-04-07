558869ecaa5cdffdab621859383f4d441b008813
Teemu Suoranta <teemu.suoranta@aucor.fi>
2016-04-07 21:33:30 +0300

    init

---
+++ polylang-copy-content.php
@@ -0,0 +1,442 @@
+<?php
+/*
+Plugin Name: Polylang Copy Content
+Plugin URI: 
+Version: 1.0
+Author: Aucor Oy, leemon
+Author URI: https://github.com/aucor
+Description: Copy content and title when creating a new translation in Polylang
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: polylang-copy-content
+*/
+
+class PolylangCopyContent {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		global $polylang;
+		
+		if (isset($polylang)) {
+			add_action('add_meta_boxes', array(&$this, 'copy_content_and_title'), 6, 2); // use this action because of parameters
+			add_filter('wp_generate_attachment_metadata', array(&$this, 'wp_generate_attachment_metadata'), 10, 2);
+		}
+	}
+
+	/**
+	 * Copy content and title 
+	 *
+	 * Send filtered content and title to editor
+	 *
+	 * @param string post_type of the translated post
+	 * @param obj post the new translated post
+	 *
+	 */
+	function copy_content_and_title($post_type, $post) {
+
+		// copying is done only when new translation is created
+		if ($GLOBALS['pagenow'] == 'post-new.php' && isset($_GET['from_post'], $_GET['new_lang'])) {
+
+			if (!PLL()->model->is_translated_post_type($post->post_type))
+				return; // post type not translatable
+
+			if(!empty($post->post_content)) {
+				return; // post content not empty, let's not mess with it
+			}
+
+			$from_post_id = (int) $_GET['from_post'];
+			$new_lang = PLL()->model->get_language($_GET['new_lang']);
+
+			$from_post_obj = get_post($from_post_id);
+
+			// copy content
+			add_filter('polylang_addon_copy_content', array(&$this, 'filter_content'), 10, 3);
+			$filtered_content = apply_filters( 'polylang_addon_copy_content', $from_post_obj->post_content, $post, $new_lang->slug );
+			$post->post_content = $filtered_content; // copy filtered content
+
+			// copy title
+			add_filter('polylang_addon_copy_title', array(&$this, 'filter_title'), 10, 2);
+			$filtered_title = apply_filters( 'polylang_addon_copy_title', $from_post_obj->post_title, $new_lang->slug );
+			$post->post_title = $filtered_title; // copy filtered title
+
+			// copy featured image
+			$this->copy_featured_image($post, $from_post_id, $new_lang->slug);
+
+			// show notice
+			add_action( 'admin_notices', function() {
+				$from_post_id = (int) $_GET['from_post'];
+			    ?>
+			    <div class="notice notice-success is-dismissible">
+			        <p><b>Success:</b> The content and title was succesfully copied from "<?php echo get_post($from_post_id)->post_title; ?>" (in <?php echo pll_get_post_language($from_post_id, 'name'); ?>).</p>
+			    </div>
+			    <?php
+			});
+		}
+	}
+
+	/**
+	 * Filter content to be inserted in translation
+	 *
+	 * @param string $content html post content
+	 * @param string $new_lang_slug slug of translated language
+	 *
+	 * @return string filtered content
+	 */
+
+	function filter_content($content, $post, $new_lang_slug) {
+
+		// if media is translatable, replace media in content with translated versions
+		if(PLL()->model->options['media_support']) {
+			add_filter('polylang_addon_copy_content_filter_img', array(&$this, 'replace_content_img'), 10, 3);
+			add_filter('polylang_addon_copy_content_filter_caption', array(&$this, 'replace_content_caption'), 10, 3);
+			add_filter('polylang_addon_copy_content_filter_caption_txt', array(&$this, 'tif_add_credit'), 10, 3);
+			add_filter('polylang_addon_copy_content_filter_gallery', array(&$this, 'replace_content_gallery'), 10, 3);
+
+			$content = apply_filters( 'polylang_addon_copy_content_filter_img', $content, $post, $new_lang_slug );
+			$content = apply_filters( 'polylang_addon_copy_content_filter_caption', $content, $post, $new_lang_slug );
+			$content = apply_filters( 'polylang_addon_copy_content_filter_gallery', $content, $post, $new_lang_slug );
+		}
+
+		return $content;
+
+	}
+
+	/**
+	 * Replace images in content with translated versions
+	 *
+	 * @param string $content html post content
+	 * @param obj $post current post object
+	 * @param string $new_lang_slug slug of translated language
+	 *
+	 * @return string filtered content
+	 */
+
+	function replace_content_img($content, $post, $new_lang_slug) {
+
+		// get all images in content (full <img> tags)
+		preg_match_all('/<img[^>]+>/i', $content, $img_array);
+
+		// no images in content
+		if(empty($img_array))
+			return $content;
+
+		// prepare nicer array structure
+		$img_and_meta = array();
+		for ($i=0; $i < count($img_array[0]); $i++) { 
+			$img_and_meta[$i] = array('tag' => $img_array[0][$i]);
+		}
+
+		foreach($img_and_meta as $i=>$arr) {
+
+			// get classes
+			preg_match('/ class="([^"]*)"/i', $img_array[0][$i], $class_temp);
+			$img_and_meta[$i]['class'] = !empty($class_temp) ? $class_temp[1] : '';
+
+			// only proceed if image is created by WordPress (has wp-image-{ID} class)
+			if(!strstr($img_and_meta[$i]['class'], 'wp-image-'))
+				continue;
+
+			// get the attachment id
+			preg_match('/wp-image-(\d+)/i', $img_array[0][$i], $id_temp);
+
+			if(empty($id_temp))
+				continue;
+
+			$img_and_meta[$i]['id'] = (int) $id_temp[1];
+
+			$attachment = get_post($img_and_meta[$i]['id']);
+
+			// check if given ID is really attachment (or copied from some other WordPress)
+			if(empty($attachment) || $attachment->post_type !== 'attachment')
+				continue;
+			
+			$img_and_meta[$i]['new_id'] = $this->translate_attachment($img_and_meta[$i]['id'], $new_lang_slug, $post->ID);
+
+			// check if already in right language
+			if($img_and_meta[$i]['new_id'] == $img_and_meta[$i]['id']) {
+				continue;
+			}
+
+			// create new class clause (don't want to risk replacing something else that has "wp-image-")
+			$img_and_meta[$i]['new_class'] = preg_replace('/wp-image-(\d+)/i', 'wp-image-' . $img_and_meta[$i]['new_id'], $img_and_meta[$i]['class']);
+			
+			// create new tag that is ready to replace the original
+			$img_and_meta[$i]['new_tag'] = preg_replace('/class="([^"]*)"/i', ' class="' . $img_and_meta[$i]['new_class'] . '"', $img_and_meta[$i]['tag']);
+
+			// replace image inside content
+			$content = str_replace($img_and_meta[$i]['tag'], $img_and_meta[$i]['new_tag'], $content);
+
+			// replace links to attachment page
+			$attachment_permalink = get_permalink( $attachment->ID );
+			if(strpos($content, $attachment_permalink) !== false) {
+				$new_attachment_permalink = get_permalink( $img_and_meta[$i]['new_id'] );
+				$content = str_replace($attachment_permalink, $new_attachment_permalink, $content);
+
+				// replace rel part as well
+				$content = str_replace('rel="attachment wp-att-' . $attachment->ID . '"', 'rel="attachment wp-att-' . $img_and_meta[$i]['new_id'] . '"', $content);
+			}
+		}
+
+		return $content;
+
+	}
+
+	/**
+	 * Replace caption shortcodes in content by setting correct attachment information
+	 * 
+	 * The <img> tags inside shortcode are replaced already by replace_content_img function
+	 *
+	 * @param string $content html post content
+	 * @param string $new_lang_slug slug of translated language
+	 *
+	 * @return string filtered content
+	 */
+
+	function replace_content_caption($content, $post, $new_lang_slug) {
+
+		preg_match_all('/\[caption(.*?)\](.*?)\[\/caption\]/i', $content, $caption_array);
+		
+		// no captions in content
+		if(empty($caption_array))
+			return $content;
+
+		// prepare nicer array structure
+		$caption_and_meta = array();
+
+		for ($i=0; $i < count($caption_array[0]); $i++) { 
+			$caption_and_meta[$i] = array('shortcode' => $caption_array[0][$i]);
+		}
+
+		foreach($caption_and_meta as $i=>$arr) {
+
+			// get ids (comma separated list)
+			preg_match('/ id="([^"]*)"/i', $caption_and_meta[$i]['shortcode'], $ids_temp);
+			$caption_and_meta[$i]['id'] = !empty($ids_temp) ? $ids_temp[1] : '';
+
+
+			// only proceed if id is in right format (attachment_{ID})
+			if(!strstr($caption_and_meta[$i]['id'], 'attachment_'))
+				continue;
+
+			// get the attachment id
+			preg_match('/attachment_(\d+)/i', $caption_and_meta[$i]['id'], $attachment_id_temp);
+
+			if(empty($attachment_id_temp))
+				continue;
+
+			$caption_and_meta[$i]['attachment_id'] = (int) $attachment_id_temp[1];
+
+			$attachment = get_post($caption_and_meta[$i]['attachment_id']);
+
+			// check if given ID is really attachment (or copied from some other WordPress)
+			if(empty($attachment) || $attachment->post_type !== 'attachment')
+				continue;
+			
+			$caption_and_meta[$i]['new_attachment_id'] = $this->translate_attachment($caption_and_meta[$i]['attachment_id'], $new_lang_slug, $post->ID);
+
+			// create new id clause (don't want to risk replacing something else that has "attachment_")
+			$caption_and_meta[$i]['new_id'] = preg_replace('/attachment_(\d+)/i', 'attachment_' . $caption_and_meta[$i]['new_attachment_id'], $caption_and_meta[$i]['id']);
+
+			// create new shortcode that is ready to replace the original
+			$caption_and_meta[$i]['new_shortcode'] = preg_replace('/ id="([^"]*)"/i', ' id="' . $caption_and_meta[$i]['new_id'] . '"', $caption_and_meta[$i]['shortcode']);
+
+			// add translation mark in caption by removing original and replacing it with the new attachment's caption
+			preg_match('/ \/>(.*?)\[\/caption\]/i', $caption_and_meta[$i]['new_shortcode'], $txt_temp);
+			$caption_and_meta[$i]['txt'] = !empty($txt_temp) ? $txt_temp[1] : '';
+
+			if(!empty($caption_and_meta[$i]['txt'])) {
+
+				$new_attachment = get_post($caption_and_meta[$i]['new_attachment_id']);
+
+				$new_caption = !empty($new_attachment->post_excerpt) ? $new_attachment->post_excerpt : '';
+
+				$caption_and_meta[$i]['new_txt'] = apply_filters( 'polylang_addon_copy_content_filter_caption_txt', $new_caption, $new_attachment, $new_lang_slug );
+
+				if(!empty($caption_and_meta[$i]['new_txt'])) {
+
+					// replace the caption in the embedded caption
+					$caption_and_meta[$i]['new_shortcode'] = preg_replace('/ \/>(.*?)\[\/caption\]/i', '/>' . $caption_and_meta[$i]['new_txt'] . '[/caption]', $caption_and_meta[$i]['new_shortcode']);
+				}
+
+			}
+
+			// replace image inside content
+			$content = str_replace($caption_and_meta[$i]['shortcode'], $caption_and_meta[$i]['new_shortcode'], $content);
+
+		}
+
+		return $content;
+
+	}
+
+	/**
+	 * Replace gallery shortcodes in content by translating attachments
+	 *
+	 * @param string $content html post content
+	 * @param string $new_lang_slug slug of translated language
+	 *
+	 * @return string filtered content
+	 */
+
+	function replace_content_gallery($content, $post, $new_lang_slug) {
+
+		preg_match_all('/\[gallery (.*?)\]/i', $content, $gallery_array);
+		
+		// no galleries in content
+		if(empty($gallery_array))
+			return $content;
+
+		// prepare nicer array structure
+		$gallery_and_meta = array();
+		for ($i=0; $i < count($gallery_array[0]); $i++) { 
+			$gallery_and_meta[$i] = array('shortcode' => $gallery_array[0][$i]);
+		}
+
+		foreach($gallery_and_meta as $i=>$arr) {
+
+			// get ids (comma separated list)
+			preg_match('/ ids="([^"]*)"/i', $gallery_and_meta[$i]['shortcode'], $ids_temp);
+			$gallery_and_meta[$i]['ids'] = !empty($ids_temp) ? $ids_temp[1] : '';
+
+			// ids empty, skip this shortcode
+			if(empty($gallery_and_meta[$i]['ids']))
+				continue;
+
+			// make id list into array for easier handeling
+			$gallery_ids_array = explode(',', str_replace(' ', '', $gallery_and_meta[$i]['ids']));
+
+			$gallery_ids_new_array = array();
+
+			// go through all images and get ids of translated attachments
+			foreach ($gallery_ids_array as $id) {
+				array_push($gallery_ids_new_array, $this->translate_attachment($id, $new_lang_slug, $post->ID));
+			}
+
+			$gallery_and_meta[$i]['ids_new'] = implode(',', $gallery_ids_new_array);
+
+			$gallery_and_meta[$i]['shortcode_new'] = preg_replace('/ ids="([^"]*)"/i', ' ids="' . $gallery_and_meta[$i]['ids_new'] . '"', $gallery_and_meta[$i]['shortcode']);
+
+			// replace galleries in content
+			$content = str_replace($gallery_and_meta[$i]['shortcode'], $gallery_and_meta[$i]['shortcode_new'], $content);
+
+		}
+
+		return $content;
+
+	}
+
+	/**
+	 * Filter title
+	 *
+	 * Add language informtion in the title (helps translators)
+	 *
+	 * @param string $content html post content
+	 * @param string $new_lang_slug slug of translated language
+	 *
+	 * @return string filtered content
+	 */
+
+	function filter_title($title, $new_lang_slug) {
+		return $title .= ' (' . $new_lang_slug . ' translation)';
+	}
+
+	/**
+	 * Copy featured image
+	 *
+	 * @param obj post new post object
+	 * @param int ID of the post we copy from
+	 * @param string slug of the new translation language
+	 */
+
+	function copy_featured_image($post, $from_post_id, $new_lang_slug) {
+		if(has_post_thumbnail( $from_post_id )) {
+			$post_thumbnail_id = get_post_thumbnail_id( $from_post_id );
+			if(PLL()->model->options['media_support']) {
+				$post_thumbnail_id = $this->translate_attachment($post_thumbnail_id, $new_lang_slug, $post->ID);
+			}
+			set_post_thumbnail( $post, $post_thumbnail_id );
+		}
+	}
+
+	/**
+	 * Translate attachment
+	 *
+	 *
+	 * @param int $attachment_id id of the attachment in original language
+	 * @param string $new_lang new language slug
+	 * @param int $parent_id id of the parent of the translated attachments (post ID)
+	 *
+	 * @return int translated id
+	 */
+	function translate_attachment($attachment_id, $new_lang, $parent_id) {
+
+		$post = get_post($attachment_id);
+		$post_id = $post->ID;
+
+		// if there's existing translation, use it
+		$existing_translation = pll_get_post($post_id, $new_lang);
+		if(!empty($existing_translation)) {
+			return $existing_translation; // existing translated attachment
+		}
+
+		$post->ID = null; // will force the creation
+		$post->post_parent = $parent_id ? $parent_id : 0;
+
+		// Append language code to caption and excerpt
+		$append_str = ' (' . $new_lang . ' translation)';
+		$post->post_excerpt = empty($post->post_excerpt) ? '' : $post->post_excerpt . $append_str;
+
+		$tr_id = wp_insert_attachment($post);
+		add_post_meta($tr_id, '_wp_attachment_metadata', get_post_meta($post_id, '_wp_attachment_metadata', true));
+		add_post_meta($tr_id, '_wp_attached_file', get_post_meta($post_id, '_wp_attached_file', true));
+
+		// copy alternative text to be consistent with title, caption and description copied when cloning the post
+		if ($meta = get_post_meta($post_id, '_wp_attachment_image_alt', true)) {
+			add_post_meta($tr_id, '_wp_attachment_image_alt', $meta);
+		}
+		
+		// set language of the attachment
+		PLL()->model->post->set_language($tr_id, $new_lang);
+		
+		$translations = PLL()->model->post->get_translations($post_id);
+		if (!$translations && $lang = PLL()->model->post->get_language($post_id)) {
+			$translations[$lang->slug] = $post_id;
+		}
+
+		$translations[$new_lang] = $tr_id;
+		PLL()->model->post->save_translations($tr_id, $translations);
+
+		return $tr_id; // newly translated attachment
+	}
+
+	/**
+	 * Generate attachment metadata
+	 *
+	 * @param int $metadata metadata of the attachment from which we copy informations
+	 * @param int $attachment_id id of the attachment to copy the metadata to
+	 */
+	function wp_generate_attachment_metadata( $metadata, $attachment_id ) {
+
+		$attachment_lang = PLL()->model->post->get_language($attachment_id);
+		
+		$translations = PLL()->model->post->get_translations($attachment_id);
+
+		foreach ($translations as $lang => $tr_id) {
+			if (!$tr_id)
+				continue;
+			
+			if ($attachment_lang->slug !== $lang) {
+				update_post_meta($tr_id, '_wp_attachment_metadata', $metadata);
+			}
+		}
+
+		return $metadata;
+
+	}
+
+}
+
+add_action('plugins_loaded', create_function('', 'global $polylang_copy_content; $polylang_copy_content = new PolylangCopyContent();'));
+
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Polylang Copy Content
+
+**Contributors:** [Teemu Suoranta](https://github.com/TeemuSuoranta), [leemon](https://github.com/theleemon) (this plugin uses bits from  [Sync Attachments for Polylang](https://github.com/theleemon/sync-attachments-for-polylang))
+**Tags:** polylang, media, attachments, admin, copy, content
+**License:** GPLv2 or later
+
+## Description
+
+Polylang Copy Content is an add-on for the multilingual WordPress plugin [Polylang](https://wordpress.org/plugins/polylang/). This add-on let's you copy the content and the title WPML style when creating a new translation. All the images and galleries are translated automatically if you use media translations.
+
+
+Basic feature list:
+
+ * Copy title and content for new translation
+ * Choose the language you want to copy from (make translation from the translated version's editor)
+ * Get useful translation markup for captions and title like (es translation) so you'll know you can overwrite it
+ * Media translation works for images, captions and galleries
+ * Make translation from featured image (if you use media translations)
+ * Use various filters to modify copied content in code
+ * Translations are done with Polylang's functions, no messing around
+
+**The plugin is still in test phase and I'd like to get feedback and tackle a few issues before going to WordPress.org. Please, report issues and contribute!**
+
+
+## Installation
+
+Download and activate. That's it. You will need Polylang first (d'oh).
+
+
+## Issues and feature whishlist
+
+**Issues:**
+
+ * Translating a link to a featured media page seems to make a broken link sometimes.
+ * HTML is parsed with regex. [This might be a problem.](http://stackoverflow.com/a/1732454) PHP Simple HTML DOM Parser can't handel shortcodes, though :(
+ * Translating featured image is somehow broken. The image can be translated and placed but if you'll try to edit the captions before the translation has been saved the first time the correct image is not selected in the media gallery.
+ * Adding translation markup (fr translation) might be someting everybody won't like. Maybe I should drop this in future or make it optional.
+
+ 
+ **Feature whishlist:**
+ * UI to handel wheteher you want to copy things and choose the language
+ * Making the plugin itself translatable
+ * Ability to copy images to translation after translation has been created (if new images are added to the original version)
+ * Undo copying (remove image translations)
+ * :star::star: Get integrated to Polylang :star::star:
+
+## Changelog
+
+### 0.1.0 - Github launch
+ * Basics working
+ * No UI
+ * Needs work before going to WordPress.org
